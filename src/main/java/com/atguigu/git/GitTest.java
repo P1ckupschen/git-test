f086168d384afc1d38cc9a9e46bb86f54729df0a
Yeap <Yeap@schen.com>
2022-10-06 09:14:35 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @author Yeah!
+ * @create 19:09
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git");
+
+
+    }
+}
